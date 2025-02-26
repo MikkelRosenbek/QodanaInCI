aecaebf4c15b86242a884a2e65efda3abde1fccc
MikkelRosenbek <miro0002@stud.kea.dk>
2025-02-26 10:25:52 +0100

    Initial commit

---
+++ src/main/java/org/example/qodanainci/QodanaInCiApplication.java
@@ -0,0 +1,13 @@
+package org.example.qodanainci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QodanaInCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QodanaInCiApplication.class, args);
+    }
+
+}
